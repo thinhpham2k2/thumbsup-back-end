63b29dd9b2b03fb8fe8632c78ab7426258ce808d
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2023-09-04 14:41:31 +0700

    First commit

---
+++ src/main/java/com/thumbsup/thumbsup/ThumbsupApplication.java
@@ -0,0 +1,13 @@
+package com.thumbsup.thumbsup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThumbsupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThumbsupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thumbsup/thumbsup/ThumbsupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thumbsup.thumbsup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThumbsupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
